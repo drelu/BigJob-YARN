8d3f4138bb9ffa847e7be8ba2a3d6d5840cd2f2d
Andre Luckow <andre.luckow@gmail.com>
2013-12-19 15:26:53 -0500

    initial version of BigJob YARN ApplicationManager based on DS example

---
+++ run.sh
@@ -0,0 +1,3 @@
+yarn -jar target/BigJob-YARN-0.1-SNAPSHOT-jar-with-dependencies.jar com.bigjob.Client --container_vcores 1\
+     -shell_command /bin/date -jar target/BigJob-YARN-0.1-SNAPSHOT-jar-with-dependencies.jar 
+
